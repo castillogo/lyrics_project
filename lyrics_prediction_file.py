7fb7f128b63489325a9304f17b559b324c99c2e7
Juan Castillo <castillogo@hotmail.com>
2020-03-19 19:33:43 +0100

    test file for lyrics prediction was changed

--- lyrics_prediction_file.py
+++ lyrics_prediction_file.py
@@ -3,12 +3,12 @@ This is a program for making a song prediction
 according to the lyrcis available in
 file output.csv
 """
-
+import re
+import warnings
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
@@ -18,41 +18,38 @@ from sklearn.metrics import confusion_matrix, f1_score
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
 from imblearn.over_sampling import RandomOverSampler
-import warnings
 warnings.filterwarnings("ignore")
 
-tv = TfidfVectorizer()
-m = MultinomialNB()
-ros = RandomOverSampler()
+TV = TfidfVectorizer()
+M = MultinomialNB()
+ROS = RandomOverSampler()
 
-lyrics_df = pd.read_csv('output.csv')
-lyrics_df['singer_number'] = lyrics_df['singer_number'].astype(int)
-lyricswords = lyrics_df['lyrics_words'].to_list()
+LYRICS_DF = pd.read_csv('output.csv')
+LYRICS_DF['singer_number'] = LYRICS_DF['singer_number'].astype(int)
+LYRICSWORDS = LYRICS_DF['lyrics_words'].to_list()
 
-def print_evaluations(y_train, y_pred, model):
+def print_evaluations(Y_TRAIN, Y_PRED, model):
 
     """
     This function summaries all scores and
     makes a confusion matrix heatman
     """
     print(f'How does model {model} score:')
-    print(f"The accuracy of the model is: {round(accuracy_score(y_train, y_pred), 3)}")
-    print(f"The precision of the model is: {round(precision_score(y_train, y_pred, average='weighted'), 3)}")
-    print(f"The recall of the model is: {round(recall_score(y_train, y_pred, average='weighted'), 3)}")
-    print(f"The f1-score of the model is: {round(f1_score(y_train, y_pred, average='weighted'), 3)}")
+    print(f"The accuracy of the model is: {round(accuracy_score(Y_TRAIN, Y_PRED), 3)}")
+    print(f"The precision of the model is: {round(precision_score(Y_TRAIN, Y_PRED, average='weighted'), 3)}")
+    print(f"The recall of the model is: {round(recall_score(Y_TRAIN, Y_PRED, average='weighted'), 3)}")
+    print(f"The f1-score of the model is: {round(f1_score(Y_TRAIN, Y_PRED, average='weighted'), 3)}")
 
     #print confusion matrix
     plt.figure(figsize=(15, 15))
-    cm = confusion_matrix(y_train, y_pred)
-    print(cm)
-    ax = plt.subplot()
-    sns.heatmap(cm, annot=True, ax = ax);
-    ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels');
-    ax.set_title('Confusion Matrix %s' % (model));
-    ax.xaxis.set_ticklabels([i for i in
-                             range(len(ynumbers))]); ax.yaxis.set_ticklabels([i
-                                  for i in range(len(ynumbers))])
-    return accuracy_score(y_train, y_pred, model)
+    Cm = confusion_matrix(Y_TRAIN, Y_PRED)
+    print(Cm)
+    Ax = plt.subplot()
+    sns.heatmap(Cm, annot=True, ax=Ax)
+    Ax.set_xlabel('Predicted labels')
+    Ax.set_ylabel('True labels')
+    Ax.set_title('Confusion Matrix %s' % (model))
+    return accuracy_score(Y_TRAIN, Y_PRED, model)
 
 if __name__ == '__main__':
 
@@ -70,27 +67,27 @@ of the file here:
     """)
     print('')
 
-    song = input()
-    song = str(song)
-    with open ("%s.txt" % (song), "r") as myfile:
-        data=myfile.readlines()
-    data=str(data)
-    data= re.sub(r"\\n", ' ', data)
+    SONG = input()
+    SONG = str(SONG)
+    with open("%s.txt" % (SONG), "r") as myfile:
+        DATA = myfile.readlines()
+    DATA = str(DATA)
+    DATA = re.sub(r"\\n", ' ', DATA)
 
     print('')
     print("""
 These are your lyrics:
     """)
     print('')
-    print(data)
+    print(DATA)
 
-    tv.fit(lyricswords)
-    tv_vectors = tv.transform(lyricswords)
-    y= lyrics_df['singer'].to_list()
-    ynumbers = lyrics_df['singer_number'].to_list()
-    m.fit(tv_vectors,ynumbers)
-    new_song=[data]
-    tv_vec = tv.transform(new_song)
+    TV.fit(LYRICSWORDS)
+    TV_VECTORS = TV.transform(LYRICSWORDS)
+    Y = LYRICS_DF['singer'].to_list()
+    YNUMBERS = LYRICS_DF['singer_number'].to_list()
+    M.fit(TV_VECTORS, YNUMBERS)
+    NEW_SONG = [DATA]
+    TV_VEC = TV.transform(NEW_SONG)
 
     #simple naive bayes
 
@@ -112,7 +109,7 @@ to this artistnumber:
     """)
     print('')
 
-    print(m.predict(tv_vec))
+    print(M.predict(TV_VEC))
 
     print('')
     print("""
@@ -121,27 +118,25 @@ song belongs to each artistnumber:
     """)
     print('')
 
-    print(m.predict_proba(tv_vec))
+    print(M.predict_proba(TV_VEC))
 
     print('')
 
-    df = pd.DataFrame(zip(lyricswords, y), columns=['lyricswords', 'ynumbers'])
-    tv_vectors.shape
+    DF = pd.DataFrame(zip(LYRICSWORDS, Y), columns=['LYRICSWORDS', 'YNUMBERS'])
 
-    y = df['ynumbers']
-    X = df[['lyricswords']]
+    Y = DF['YNUMBERS']
+    X = DF[['LYRICSWORDS']]
 
-    X_resample, y_resample = ros.fit_resample(X, y)
-    X_resample
+    X_RESAMPLE, Y_RESAMPLE = ROS.fit_resample(X, Y)
 
-    cv = CountVectorizer(ngram_range=(1,1))
-    cv.fit(lyricswords)
+    CV = CountVectorizer(ngram_range=(1, 1))
+    CV.fit(LYRICSWORDS)
 
-    word_vectors = cv.transform(lyricswords)
-    cv.get_feature_names()
-    df2 = pd.DataFrame(word_vectors.todense(), columns=cv.get_feature_names())
-    X = df2
-    y = df['ynumbers']
+    WORD_VECTORS = CV.transform(LYRICSWORDS)
+    CV.get_feature_names()
+    DF2 = pd.DataFrame(WORD_VECTORS.todense(), columns=CV.get_feature_names())
+    X = DF2
+    Y = DF['YNUMBERS']
 
     print('')
     print("""
@@ -150,21 +145,21 @@ for a baseline model:
     """)
     print('')
 
-    split=0.1
-    X_train, X_test, y_train, y_test = train_test_split(X, 
-                                                        ynumbers, 
-                                                        random_state=10, 
-                                                        test_size=split)
+    SPLIT = 0.1
+    X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X,
+                                                        YNUMBERS,
+                                                        random_state=10,
+                                                        test_size=SPLIT)
 
     #Baseline model
 
-    ypred_bl = [0] * X_train.shape[0]
-    print_evaluations(y_train, ypred_bl, 'Baseline')
-    new_df = pd.concat([X, y], axis = 1)
-    new_df.groupby('ynumbers').size()
-    new_df.groupby('ynumbers').size()[1]/new_df.shape[0]*100
-    X = new_df.iloc[:,:-1]
-    y = new_df.ynumbers
+    YPRED_BL = [0] * X_TRAIN.shape[0]
+    print_evaluations(Y_TRAIN, YPRED_BL, 'Baseline')
+    NEW_DF = pd.concat([X, Y], axis=1)
+    NEW_DF.groupby('YNUMBERS').size()
+    #NEW_DF.groupby('YNUMBERS').size()[1]/NEW_DF.shape[0]*100
+    X = NEW_DF.iloc[:, :-1]
+    Y = NEW_DF.YNUMBERS
 
     # simple Random forest model
     print('')
@@ -173,10 +168,10 @@ These are the results of the
 random forest evaluation:
     """)
 
-    rf = RandomForestClassifier(n_estimators=20, max_depth=3, random_state=10)
-    rf.fit(X_train, y_train)
-    ypred_rf = rf.predict(X_test)
-    rf2=rf
+    RF = RandomForestClassifier(n_estimators=20, max_depth=3, random_state=10)
+    RF.fit(X_TRAIN, Y_TRAIN)
+    YPRED_RF = RF.predict(X_TEST)
+    RF2 = RF
 
     print('')
     print("""
@@ -185,7 +180,7 @@ for the artist number for your song:
     """)
     print('')
 
-    print(rf.predict(tv_vec))
+    print(RF.predict(TV_VEC))
 
     print("""
 These are the probabilities that
@@ -193,7 +188,7 @@ your song belongs to each artistnumber:
     """)
     print('')
 
-    print(rf.predict_proba(tv_vec))
+    print(RF.predict_proba(TV_VEC))
 
     print('')
     print("""
@@ -202,15 +197,15 @@ for the train-test split:
     """)
     print('')
 
-    print_evaluations(y_test, ypred_rf, 'RandomForest')
+    print_evaluations(Y_TEST, YPRED_RF, 'RandomForest')
 
     # Random oversampling model
 
-    ros = RandomOverSampler(random_state=10)
-    X_ros, y_ros = ros.fit_resample(X_train, y_train)
-    np.unique(y_ros, return_counts=True)
-    rf2.fit(X_ros, y_ros)
-    ypred_ros = rf2.predict(X_test)
+    ROS = RandomOverSampler(random_state=10)
+    X_ROS, Y_ROS = ROS.fit_resample(X_TRAIN, Y_TRAIN)
+    np.unique(Y_ROS, return_counts=True)
+    RF2.fit(X_ROS, Y_ROS)
+    YPRED_ROS = RF2.predict(X_TEST)
 
     print('')
     print("""
@@ -220,7 +215,7 @@ evaluation for your song:
     """)
     print('')
 
-    print(rf2.predict(tv_vec))
+    print(RF2.predict(TV_VEC))
 
     print('')
     print("""
@@ -228,26 +223,26 @@ These are the probabilities that
 your song belongs to each artistnumber:
     """)
     print('')
-    print(rf2.predict_proba(tv_vec))
+    print(RF2.predict_proba(TV_VEC))
     print('')
     print("""
 These are the random oversampling
 evaluations with the train-test split:
     """)
     print('')
-    print_evaluations(y_test, ypred_ros, 'RandomOversampling')
+    print_evaluations(Y_TEST, YPRED_ROS, 'RandomOversampling')
 
 
-    y= lyrics_df['singer'].to_list()
-    ynumbers = lyrics_df['singer_number'].to_list()
-    artistlistfinal = dict(zip(y, ynumbers))
+    Y = LYRICS_DF['singer'].to_list()
+    YNUMBERS = LYRICS_DF['singer_number'].to_list()
+    ARTISTLISTFINAL = dict(zip(Y, YNUMBERS))
 
     print('')
     print("""
 This is the code for the artists
 and the belonging artistnumbers:
     """)
-    print(artistlistfinal)
+    print(ARTISTLISTFINAL)
     print('')
     print("""
 These are the heatmaps for the confusion
--- test_lyrics_prediction_file.py
+++ test_lyrics_prediction_file.py
@@ -5,9 +5,11 @@ Created on Mon Mar 16 15:59:40 2020
 @author: casti
 """
 
-# import pytest
+import pytest
 from lyrics_prediction_file import print_evaluations
 
-#artist='Oasis'
+Y_TEST = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+Y_PRED = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 def test_print_evaluations():
-    assert print_evaluations(y_test, ypred_rf, 'Randomforest') >= 0.3
+    assert print_evaluations(Y_TEST, Y_PRED, 'Randomforest') > 0
