ee1182f7f885e7017f61232ae63a1029b7370a60
Juan Castillo <castillogo@hotmail.com>
2020-03-20 13:42:04 +0100

    changes in test folder

---
+++ tests/test_lyrics_scraping_file.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 15:59:40 2020
+
+@author: casti
+"""
+
+import pytest
+from lyrics_scraping_file import souploop, lyricsmodification
+
+def test_artist():
+    ARTISTLIST = ['Aerosmith']
+    for artist in ARTISTLIST:
+        assert lyricsmodification(souploop(artist), artist, ARTISTLIST)[0] >= 100
